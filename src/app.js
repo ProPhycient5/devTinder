e3a033e37f0b6854df1426d35406b9dad3804d98
ProPhycient <dsawan.office8@gmail.com>
2025-09-28 10:40:28 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,15 +2,31 @@ const express = require('express');
 
 const app = express();
 
-app.use("/",(req, res) => {
-    res.send("Hello from default server")             //server requestor
+// app.use("/",(req, res) => {
+//     res.send("Hello from default server")             //server requestor
+// });
+
+app.get("/user", (req, res) => {
+    res.send("Fetched user")             
 });
 
-app.use("/test",(req, res) => {
-    res.send("Hello from test server")             //server requestor
+app.post("/user", (req, res) => {
+    res.send({ name: "Sawan", gender: "MALE" })           
 });
 
-app.use("/dashboard",(req, res) => {
+app.delete("/user", (req, res) => {
+    res.send("Deleted succussfully");          
+});
+
+app.put("/user", (req, res) => {
+    res.send("Updated successfully");      
+});
+
+app.use("/test/3", (req, res) => {
+    res.send("Hello from 3")            
+});
+
+app.use("/dashboard", (req, res) => {
     res.send("Hello from dashboard")             //server requestor
 });
 
